3be8f96864299c8321b7247c41a3fa1dd1294fac
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-12-11 17:33:53 -0600

    Completed 'project 0'
    Now loads input files and prints the source listing

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+import static java.lang.System.out;
+
+
 public class Parser {
 
-  public Parser(String filepath) {
-    System.out.println("Hello world at" + filepath);
+  ArrayList<String> sourceBuffer = new ArrayList<String>();
+
+  public Parser(Scanner file) {
+    while (file.hasNext()) {
+      // Read source into buffer
+      // Per project spec, only consider upto 72 characters per line.
+      String line = file.nextLine();
+      sourceBuffer.add(line.substring(0, Math.min(72, line.length())));
+    }
+    file.close();
   }
+
+  public void computeProjectZero() {
+    for (int i = 0; i < sourceBuffer.size(); i++) {
+      out.println(i + ". " + sourceBuffer.get(i));
+    }
+  }
+
+  public static Scanner getFile(String filepath) {
+    try {
+      return new Scanner(new BufferedReader(new FileReader(filepath)));
+    } catch (FileNotFoundException e) {
+      out.println(filepath + " Not Found");
+
+      e.printStackTrace();
+      return null;
+    }
+  }
+
 }
--- src/Project0.java
+++ src/Project0.java
@@ -2,7 +2,8 @@ public class Project0 {
 
   public static void main(String[] args) {
 
-    Parser p = new Parser("placeholder file");
+    Parser p = new Parser(Parser.getFile("input/sampleFromGrammar.pas"));
+    p.computeProjectZero();
   }
 
 }
