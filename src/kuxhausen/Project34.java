684f9f266e4415875449f88247887bb96bb264e5
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-12-19 05:49:55 -0600

    added project 3/4 runner

---
+++ src/kuxhausen/Project34.java
@@ -0,0 +1,23 @@
+package kuxhausen;
+
+import java.util.Scanner;
+
+/**
+ * @author Eric Kuxhausen
+ */
+public class Project34 {
+  public static void main(String[] args) {
+    for (String filename : args) {
+      Scanner file = Lexar.getFile("input/" + filename + ".pas");
+      if (file != null) {
+        Lexar l = new Lexar(file);
+        DecoratedParser p = new DecoratedParser(l);
+
+        Utils.writeListingFile("output/" + filename + ".listing", p.getTokenList(),
+            l.getSourceBuffer());
+        Utils.writeTokenFile("output/" + filename + ".token", p.getTokenList());
+
+      }
+    }
+  }
+}
