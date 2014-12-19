0e66c488af0585cc4639dc5f66c2d0dea258da74
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2014-12-19 00:24:59 -0600

    started writing test samples for project3/4

---
+++ input/semanticTest1.pas
@@ -0,0 +1,13 @@
+program example(input, output);
+var x: integer ;
+var y: integer ;
+var z: real ;
+procedure gcd(a : integer ; b: real );
+begin
+	if (b>0.0) or (x<>y) then x:=2 mod a
+	else z:= b/2.0
+end ;
+begin
+	x := 5;
+	call gcd(x, y)
+end .
