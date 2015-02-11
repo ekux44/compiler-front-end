674488e1b5c04f7042aea1c11d694f4e8ffdf4d2
Eric Kuxhausen <erickuxhausen+git@gmail.com>
2015-02-11 10:07:17 -0600

    test program changes

--- input/cor3.pas
+++ input/cor3.pas
@@ -3,6 +3,7 @@ program test (input, output);
   var a : integer;
   var b : real;
   var c : array [1..2] of integer;
+  var d : real;
 
   procedure proc1(x:integer; y:real; 
                   z:array [1..2] of integer; q: real);
@@ -33,17 +34,16 @@ program test (input, output);
      begin
        call proc1(x, e, c, b);
        call proc3(c[1], e);
-       e := e + 4.44;
-       a:= (a mod y) div x;
-       while ((a >= 4) and ((b <= e) 
+       e := e + 4;
+       a:= (a mod 4.4) div 4.4;
+       while ((a >= 4.4) and ((b <= e) 
                        or (not (a = c[a])))) do
          begin
-           a:= c[a] + 1
+           a:= c + 1.0 + $
          end
      end;
 
 begin
-  call proc2(c[4], c[5]);
   call proc2(c[4],2);
   if (a < 2) then a:= 1 else a := a + 2;
   if (b > 4.2) then a := c[a]
